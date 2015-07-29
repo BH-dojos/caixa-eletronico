59987dee873535cc68047ed63fce6b3c7c962641
Conference <conference@TechOps-BH.local>
2015-07-29 20:44:47 -0300

    refatoracao para incluir classe

--- dojo.py
+++ dojo.py
@@ -1,26 +1,28 @@
-def obter_menor_numero_de_notas(valor):
-    saldo = valor
-    resultado = []
-    while saldo > 0:
-        nota_atual = _maior_nota_que_cabe_no(saldo)
-        saldo -= nota_atual
-        resultado.append(nota_atual)
+class CaixaEletronico():
+
+    def obter_menor_numero_de_notas(self, valor):
+        saldo = valor
+        resultado = []
+        while saldo > 0:
+            nota_atual = self._maior_nota_que_cabe_no(saldo)
+            saldo -= nota_atual
+            resultado.append(nota_atual)
 
 
-    return resultado
+        return resultado
 
 
-def _maior_nota_que_cabe_no(saldo):
-    if saldo >= 100:
-        return 100
-    elif saldo >= 50:
-        return 50
-    elif saldo >= 20:
-        return 20
-    elif saldo >=10:
-        return 10
-    else:
-        raise DojoExcecao
+    def _maior_nota_que_cabe_no(self, saldo):
+        if saldo >= 100:
+            return 100
+        elif saldo >= 50:
+            return 50
+        elif saldo >= 20:
+            return 20
+        elif saldo >=10:
+            return 10
+        else:
+            raise DojoExcecao
 
 class DojoExcecao(Exception):
     pass
--- test_dojo.py
+++ test_dojo.py
@@ -1,46 +1,47 @@
 import unittest
-import dojo
+from dojo import CaixaEletronico, DojoExcecao
 
 class TestDojo(unittest.TestCase):
 
     def setUp(self):
+        self.caixa = CaixaEletronico()
         pass
 
     def tearDown(self):
         pass
 
     def test_notas_para_100_eh_uma_de_100(self):
-        self.assertEqual(dojo.obter_menor_numero_de_notas(100), [100])
+        self.assertEqual(self.caixa.obter_menor_numero_de_notas(100), [100])
 
     def test_notas_para_50_eh_uma_de_50(self):
-        self.assertEqual(dojo.obter_menor_numero_de_notas(50), [50])
+        self.assertEqual(self.caixa.obter_menor_numero_de_notas(50), [50])
 
     def test_notas_para_20_eh_uma_de_20(self):
-        self.assertEqual(dojo.obter_menor_numero_de_notas(20), [20])
+        self.assertEqual(self.caixa.obter_menor_numero_de_notas(20), [20])
 
     def test_notas_para_10_eh_uma_de_10(self):
-        self.assertEqual(dojo.obter_menor_numero_de_notas(10), [10])
+        self.assertEqual(self.caixa.obter_menor_numero_de_notas(10), [10])
 
     def test_notas_para_30_sao_uma_de_10_e_uma_de_20(self):
-        self.assertEqual(dojo.obter_menor_numero_de_notas(30), [20, 10])
+        self.assertEqual(self.caixa.obter_menor_numero_de_notas(30), [20, 10])
 
     def test_notas_para_120_sao_uma_de_100_e_uma_de_20(self):
-        self.assertEqual(dojo.obter_menor_numero_de_notas(120), [100, 20])
+        self.assertEqual(self.caixa.obter_menor_numero_de_notas(120), [100, 20])
 
     def test_notas_para_500_sao_cinco_de_100(self):
-        self.assertEqual(dojo.obter_menor_numero_de_notas(500), [100, 100, 100, 100, 100])
+        self.assertEqual(self.caixa.obter_menor_numero_de_notas(500), [100, 100, 100, 100, 100])
 
     def test_notas_para_130_sao_uma_100_uma_20_e_uma_10(self):
-        self.assertEqual(dojo.obter_menor_numero_de_notas(130), [100, 20, 10])
+        self.assertEqual(self.caixa.obter_menor_numero_de_notas(130), [100, 20, 10])
 
     def test_notas_para_9_levanta_excecao_nao_permite_sacar(self):
-        self.assertRaises(dojo.DojoExcecao, dojo.obter_menor_numero_de_notas, 9)
+        self.assertRaises(DojoExcecao, self.caixa.obter_menor_numero_de_notas, 9)
 
     def test_notas_para_180_sao_uma_100_uma_50_uma_20_e_uma_10(self):
-        self.assertEqual(dojo.obter_menor_numero_de_notas(180), [100,50, 20, 10])
+        self.assertEqual(self.caixa.obter_menor_numero_de_notas(180), [100,50, 20, 10])
 
     def test_notas_para_101_levanta_excecao_nao_permite_sacar(self):
-        self.assertRaises(dojo.DojoExcecao, dojo.obter_menor_numero_de_notas, 101)
+        self.assertRaises(DojoExcecao, self.caixa.obter_menor_numero_de_notas, 101)
 
 
 if __name__ == 'main':
